56d3a588338693b41ffa94bf87df0334fcca8fb3
devnevo <pavansb@protonmail.com>
2018-08-26 03:04:07 +0530

    changes

--- routes/api/users.js
+++ routes/api/users.js
@@ -7,36 +7,32 @@ const bcrypt = require("bcryptjs");
 
 router.get("/test", (req, res) => res.json({ msg: "connected to Users" }));
 router.post("/register", (req, res) => {
-  User.findOne({ email: res.body.email }).then(user => {
+  User.findOne({ email: req.body.email }).then(user => {
     if (user) {
-      return res.status(404).json({ email: "Email Already Exists" });
+      return res.status(400).json({ email: "Email Already Exists" });
     } else {
-      const avatar = gravatar(res.body.email, {
+      const avatar = gravatar.url(req.body.email, {
         s: "200",
         r: "pg",
         d: "mm"
       });
       const newUser = new User({
-        name: res.body.name,
-        email: res.body.email,
+        name: req.body.name,
+        email: req.body.email,
         avatar,
-        password: res.body.password
+        password: req.body.password
       });
 
-      bcrypt.genSalt(
-        10,
-        (err,
-        salt => {
-          bcrypt.hash(newUser.password, salt, (err, hash) => {
-            if (err) throw err;
-            newUser.password = hash;
-            newUser
-              .save()
-              .then(user => user.json(user))
-              .catch(err => console.log(err));
-          });
-        })
-      );
+      bcrypt.genSalt(10, (err, salt) => {
+        bcrypt.hash(newUser.password, salt, (err, hash) => {
+          if (err) throw err;
+          newUser.password = hash;
+          newUser
+            .save()
+            .then(user => res.json(user))
+            .catch(err => console.log(err));
+        });
+      });
     }
   });
 });
