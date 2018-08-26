78f9911d35592c3448b99ebc79a33d3654eed1ec
devnevo <pavansb@protonmail.com>
2018-08-26 19:28:56 +0530

    user login changes

--- routes/api/users.js
+++ routes/api/users.js
@@ -37,4 +37,23 @@ router.post("/register", (req, res) => {
   });
 });
 
+router.post("/login", (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  User.findOne({ email }).then(user => {
+    if (!user) {
+      return res.status(404).json({ email: "Email Id does not exist" });
+    }
+
+    bcrypt.compare(password, user.password).then(isMatch => {
+      if (isMatch) {
+        res.json({ msg: "Success" });
+      } else {
+        return res.status(400).json({ password: "Password Incorrect" });
+      }
+    });
+  });
+});
+
 module.exports = router;
