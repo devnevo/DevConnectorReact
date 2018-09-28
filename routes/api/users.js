27d8adcc263c41f5d25659714f9d3e66e05c5ce9
devnevo <pavansb@protonmail.com>
2018-09-28 18:58:51 +0530

    validation changes

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,5 +1,4 @@
 const express = require("express");
-
 const router = express.Router();
 const User = require("../../models/User");
 const gravatar = require("gravatar");
@@ -7,12 +6,18 @@ const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const keys = require("../../config/keys");
 const passport = require("passport");
+const validateRegisterInput = require("../../validation/register");
 
 router.get("/test", (req, res) => res.json({ msg: "connected to Users" }));
 router.post("/register", (req, res) => {
+  const { errors, isValid } = validateRegisterInput(req.body);
+  if (!isValid) {
+    return res.status(404).json(errors);
+  }
   User.findOne({ email: req.body.email }).then(user => {
     if (user) {
-      return res.status(400).json({ email: "Email Already Exists" });
+      errors.email = "Email Already Exists";
+      return res.status(400).json(errors);
     } else {
       const avatar = gravatar.url(req.body.email, {
         s: "200",
