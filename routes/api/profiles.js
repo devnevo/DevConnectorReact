18eddf247ef5d792872436de502192888a05d7db
devnevo <pavansb@protonmail.com>
2018-10-18 01:05:38 +0530

    get call for profile with handle

--- routes/api/profiles.js
+++ routes/api/profiles.js
@@ -8,12 +8,27 @@ const validateProfileInput = require("../../validation/profiles");
 
 router.get("/test", (req, res) => res.json({ msg: "connected to Profiles" }));
 
+router.get("/handle/:handle", (req, res) => {
+  const errors = {};
+  Profile.findOne({ handle: req.params.handle })
+    .populate("user", ["name", "avatar"])
+    .then(profile => {
+      if (!profile) {
+        errors.noProfile = "There is no profie connected to this handle";
+        res.status(404).json(errors);
+      }
+      res.json(profile);
+    })
+    .catch(err => res.status(404).json(err));
+});
+
 router.get(
   "/",
   passport.authenticate("jwt", { session: false }),
   (req, res) => {
     const errors = {};
     Profile.findOne({ user: req.user.id })
+      .populate("user", ["name", "avatar"])
       .then(profile => {
         if (!profile) {
           errors.noProfile = "User profile does not exist";
