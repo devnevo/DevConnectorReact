5adabd46f5346ae5e966e8e9fef81d2d891b7f17
devnevo <pavansb@protonmail.com>
2018-11-11 23:17:25 +0530

    api integration for login and register routes

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,7 +1,40 @@
 import React, { Component } from "react";
+import classnames from "classname";
+import axios from "axios";
 
 class Login extends Component {
+  constructor() {
+    super();
+    this.state = {
+      email: "",
+      password: "",
+      errors: {}
+    };
+
+    this.onChange = this.onChange.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+  }
+
+  onChange(e) {
+    this.setState({ [e.target.name]: e.target.value });
+  }
+
+  onSubmit(e) {
+    e.preventDefault();
+
+    const User = {
+      email: this.state.email,
+      password: this.state.password
+    };
+
+    axios
+      .post("/api/users/login", User)
+      .then(res => console.log(res.data))
+      .catch(err => this.setState({ errors: err.response.data }));
+  }
+
   render() {
+    const { errors } = this.state;
     return (
       <div className="login">
         <div className="container">
@@ -11,22 +44,36 @@ class Login extends Component {
               <p className="lead text-center">
                 Sign in to your DevConnector account
               </p>
-              <form action="dashboard.html">
+              <form onSubmit={this.onSubmit}>
                 <div className="form-group">
                   <input
                     type="email"
-                    className="form-control form-control-lg"
+                    className={classnames("form-control form-control-lg", {
+                      "is-invalid": errors.email
+                    })}
                     placeholder="Email Address"
                     name="email"
+                    value={this.state.email}
+                    onChange={this.onChange}
                   />
+                  {errors.email && (
+                    <div className="invalid-feedback">{errors.email}</div>
+                  )}
                 </div>
                 <div className="form-group">
                   <input
                     type="password"
-                    className="form-control form-control-lg"
+                    className={classnames("form-control form-control-lg", {
+                      "is-invalid": errors.password
+                    })}
                     placeholder="Password"
                     name="password"
+                    value={this.state.password}
+                    onChange={this.onChange}
                   />
+                  {errors.password && (
+                    <div className="invalid-feedback">{errors.password}</div>
+                  )}
                 </div>
                 <input type="submit" className="btn btn-info btn-block mt-4" />
               </form>
--- validation/register.js
+++ validation/register.js
@@ -9,7 +9,7 @@ module.exports = function validateRegisterInput(data) {
   data.password2 = !isEmpty(data.password2) ? data.password2 : "";
 
   if (!Validator.isLength(data.name, { min: 3, max: 30 })) {
-    errors.name = "name must be between 2 and 30 characters";
+    errors.name = "Name must be between 2 and 30 characters";
   }
 
   if (Validator.isEmpty(data.name)) {
@@ -25,19 +25,19 @@ module.exports = function validateRegisterInput(data) {
   }
 
   if (Validator.isEmpty(data.password)) {
-    errors.password = "password field if missing";
+    errors.password = "Password field if missing";
   }
 
   if (Validator.isEmpty(data.password2)) {
-    errors.password2 = "password field if missing";
+    errors.password2 = "Password field if missing";
   }
 
   if (!Validator.isLength(data.password, { min: 6, max: 30 })) {
-    errors.password = "password must be of length 6 to 30 characters";
+    errors.password = "Password must be of length 6 to 30 characters";
   }
 
   if (!Validator.equals(data.password, data.password2)) {
-    errors.name = "confirm password does not match the password";
+    errors.password2 = "Confirm password does not match the password";
   }
 
   return {
