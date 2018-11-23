42523d6972b19955ebc0844bc81c349687c7d251
devnevo <pavansb@protonmail.com>
2018-11-23 16:51:58 +0530

    actions and reducer additions

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const passport = require("passport");
+const users = require("./routes/api/users");
+const profiles = require("./routes/api/profiles");
+const posts = require("./routes/api/posts");
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+//DB Props
+const db = require("./config/keys").mongodbURI;
+
+//connect to Db
+mongoose
+  .connect(db)
+  .then(() => console.log("you are connected to mongoDB"))
+  .catch(err => console.log(err));
+
+app.use(passport.initialize());
+
+require("./config/passport")(passport);
+
+//routing to files
+app.use("/api/posts", posts);
+app.use("/api/profiles", profiles);
+app.use("/api/users", users);
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server runnig on port ${port}`));
