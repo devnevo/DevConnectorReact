aa5e1c48dec63769a4f15944057144bc463c2a98
PAVAN SAI <36514380+devnevo@users.noreply.github.com>
2018-11-23 18:48:04 +0530

    Update register.js

--- validation/register.js
+++ validation/register.js
@@ -9,15 +9,7 @@ module.exports = function validateRegisterInput(data) {
   data.password2 = !isEmpty(data.password2) ? data.password2 : "";
 
   if (!Validator.isLength(data.name, { min: 3, max: 30 })) {
-<<<<<<< HEAD
-<<<<<<< HEAD
     errors.name = "Name must be between 2 and 30 characters";
-=======
-    errors.name = "name must be between 2 and 30 characters";
->>>>>>> 90bf76297c61a7bdd143e664c671fea10a38a393
-=======
-    errors.name = "Name must be between 2 and 30 characters";
->>>>>>> origin/develop
   }
 
   if (Validator.isEmpty(data.name)) {
@@ -33,8 +25,6 @@ module.exports = function validateRegisterInput(data) {
   }
 
   if (Validator.isEmpty(data.password)) {
-<<<<<<< HEAD
-<<<<<<< HEAD
     errors.password = "Password field if missing";
   }
 
@@ -48,30 +38,7 @@ module.exports = function validateRegisterInput(data) {
 
   if (!Validator.equals(data.password, data.password2)) {
     errors.password2 = "Confirm password does not match the password";
-=======
-    errors.password = "password field if missing";
-=======
-    errors.password = "Password field if missing";
->>>>>>> origin/develop
   }
-
-  if (Validator.isEmpty(data.password2)) {
-    errors.password2 = "Password field if missing";
-  }
-
-  if (!Validator.isLength(data.password, { min: 6, max: 30 })) {
-    errors.password = "Password must be of length 6 to 30 characters";
-  }
-
-  if (!Validator.equals(data.password, data.password2)) {
-<<<<<<< HEAD
-    errors.name = "confirm password does not match the password";
->>>>>>> 90bf76297c61a7bdd143e664c671fea10a38a393
-=======
-    errors.password2 = "Confirm password does not match the password";
->>>>>>> origin/develop
-  }
-
   return {
     errors,
     isValid: isEmpty(errors)
